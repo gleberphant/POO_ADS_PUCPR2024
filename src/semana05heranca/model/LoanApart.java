d987663143faedabbc679c1df967b6d078725cd5
gleberphant <gleberphant@gmail.com>
2024-10-29 01:01:36 -0300

    atividade 4 - id automatico dentro do builder
    Signed-off-by: gleberphant <gleberphant@gmail.com>

---
+++ src/semana05heranca/model/LoanApart.java
@@ -0,0 +1,4 @@
+package semana05heranca.model;
+
+public class LoanHouse {
+}
---
+++ src/semana05heranca/model/LoanHouse.java
@@ -0,0 +1,24 @@
+package semana05heranca.model;
+
+public class LoanApartment extends Loan{
+    private final double INSURANCE_VALUE;
+
+    /**
+     * Construtor
+     *
+     * @param id
+     * @param price O preço do bem a ser financiado.
+     * @param term  O prazo do financiamento em meses.
+     * @param fee   A taxa de juros do financiamento.
+     * @throws IllegalArgumentException Se o preço, o prazo ou a taxa forem inválidos.
+     */
+    protected LoanApartment(int id, double price, int term, double fee) throws IllegalArgumentException {
+        super(id, price, term, fee);
+        INSURANCE_VALUE = 80;
+    }
+
+    @Override
+    public double getPaymentValueMonthly() {
+        return super.getPaymentValueMonthly() + INSURANCE_VALUE;
+    }
+}
